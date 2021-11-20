dfdd7bdc2fc4d9605f2287a6821de72c4ed33b37
Daniel Silva <dani3l.dxp@gmail.com>
2021-11-20 13:38:17 -0300

    feat: add google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,27 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
         <title>Daniel Silva &bull; Front-end Developer</title>
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="shortcut icon"
-          href="/img/icon-512.png"
+          href="./img/icon-512.png"
           type="image/x-icon"
         />
         <link
           rel="apple-touch-icon"
-          href="/img/icon-512.png"
+          href="./img/icon-512.png"
           type="image/x-icon"
         />
         <link rel="manifest" href="/manifest.json" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200;300;400;500;600;700&family=Playfair+Display:wght@400;600;700&family=Poppins:wght@200;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
