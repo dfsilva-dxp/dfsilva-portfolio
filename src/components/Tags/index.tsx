a3df58bb845182924a8245e330e4684bfea94600
Daniel Silva <dani3l.dxp@gmail.com>
2021-11-22 15:41:17 -0300

    feat: implementing the tag

---
+++ src/components/Tags/index.tsx
@@ -0,0 +1,16 @@
+import * as S from "./styles";
+
+const Tags = () => {
+  return (
+    <S.Tags>
+      <span className="tags top-tags"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+      <span className="tags bottom-tags">
+        &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+        <br />
+        &lt;/html&gt;
+      </span>
+    </S.Tags>
+  );
+};
+
+export default Tags;
