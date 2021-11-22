1c7adafda388cdd10b889f4d9564a8cf0172e6d6
Daniel Silva <dani3l.dxp@gmail.com>
2021-11-22 12:19:06 -0300

    feat: implementing the header aplication

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,37 @@
+import { useCallback, useState } from "react";
+
+import BurgerButton from "components/BurgerButton";
+import FullScreenMenu from "components/FullScreenMenu";
+
 import * as S from "./styles";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleIsActiveMenu = useCallback(
+    () => setIsActive(!isActive),
+    [isActive]
+  );
+
   return (
-    <S.Wrapper>
-      <h1>Header</h1>
-    </S.Wrapper>
+    <>
+      <S.Header>
+        <div className="container">
+          <S.Logo>
+            daniel
+            <span>silva</span>
+          </S.Logo>
+          <BurgerButton
+            toggleIsActiveMenu={toggleIsActiveMenu}
+            isActive={isActive}
+          />
+        </div>
+      </S.Header>
+      <FullScreenMenu
+        toggleIsActiveMenu={toggleIsActiveMenu}
+        isActive={isActive}
+      />
+    </>
   );
 };
 
