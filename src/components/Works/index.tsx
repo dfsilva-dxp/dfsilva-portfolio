df965ea06eee42787119eab7bb265b6748c43bbe
Daniel Silva <dani3l.dxp@gmail.com>
2021-12-03 09:24:06 -0300

    feat: ajustando o parallax do projeto

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import SwiperCore, { Pagination } from "swiper";
+import SwiperCore, { Parallax, Pagination, Navigation } from "swiper";
 
 // install Swiper modules
-SwiperCore.use([Pagination]);
+SwiperCore.use([Parallax, Pagination, Navigation]);
 
 import * as S from "./styles";
 
@@ -18,6 +18,8 @@ const WorksWrapper = () => {
             centeredSlides={true}
             loop={true}
             speed={1000}
+            parallax={true}
+            navigation={true}
             breakpoints={{
               320: {
                 slidesPerView: 1,
@@ -34,13 +36,71 @@ const WorksWrapper = () => {
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
+            <div
+              slot="container-start"
+              className="parallax-bg"
+              data-swiper-parallax="-23%"
+            ></div>
+            <SwiperSlide>
+              <div className="title" data-swiper-parallax="-300">
+                Slide 1
+              </div>
+              <div className="subtitle" data-swiper-parallax="-200">
+                Subtitle
+              </div>
+              <div className="text" data-swiper-parallax="-200">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam dictum mattis velit, sit amet faucibus felis iaculis
+                  nec. Nulla laoreet justo vitae porttitor porttitor.
+                  Suspendisse in sem justo. Integer laoreet magna nec elit
+                  suscipit, ac laoreet nibh euismod. Aliquam hendrerit lorem at
+                  elit facilisis rutrum. Ut at ullamcorper velit. Nulla ligula
+                  nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean
+                  feugiat non eros quis feugiat.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="title" data-swiper-parallax="-300">
+                Slide 2
+              </div>
+              <div className="subtitle" data-swiper-parallax="-200">
+                Subtitle
+              </div>
+              <div className="text" data-swiper-parallax="-100">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam dictum mattis velit, sit amet faucibus felis iaculis
+                  nec. Nulla laoreet justo vitae porttitor porttitor.
+                  Suspendisse in sem justo. Integer laoreet magna nec elit
+                  suscipit, ac laoreet nibh euismod. Aliquam hendrerit lorem at
+                  elit facilisis rutrum. Ut at ullamcorper velit. Nulla ligula
+                  nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean
+                  feugiat non eros quis feugiat.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="title" data-swiper-parallax="-300">
+                Slide 3
+              </div>
+              <div className="subtitle" data-swiper-parallax="-200">
+                Subtitle
+              </div>
+              <div className="text" data-swiper-parallax="-100">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam dictum mattis velit, sit amet faucibus felis iaculis
+                  nec. Nulla laoreet justo vitae porttitor porttitor.
+                  Suspendisse in sem justo. Integer laoreet magna nec elit
+                  suscipit, ac laoreet nibh euismod. Aliquam hendrerit lorem at
+                  elit facilisis rutrum. Ut at ullamcorper velit. Nulla ligula
+                  nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean
+                  feugiat non eros quis feugiat.
+                </p>
+              </div>
+            </SwiperSlide>
           </Swiper>
         </div>
       </div>
--- src/components/Works/styles.ts
+++ src/components/Works/styles.ts
@@ -10,7 +10,8 @@ export const Wrapper = styled.section`
   }
   .swiper {
     width: 100%;
-    height: 480px;
+    height: 70vh;
+    margin: 15vh 0 0;
   }
 
   .swiper-slide {
@@ -19,16 +20,55 @@ export const Wrapper = styled.section`
     font-size: 18px;
     background: #fff;
     color: red;
+    padding: 0px 15px;
 
     /* Center slide text vertically */
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+  }
+
+  .parallax-bg {
+    border: 1px solid #ff0;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 130%;
+    height: 100%;
+    -webkit-background-size: cover;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .swiper-slide .title {
+    font-size: 41px;
+    font-weight: 300;
+  }
+
+  .swiper-slide .subtitle {
+    font-size: 21px;
+  }
+
+  .swiper-slide .text {
+    font-size: 14px;
+    max-width: 400px;
+    line-height: 1.3;
+  }
+
+  .swiper-slide-prev .text {
+    opacity: 0;
+    transform: translateX(-40px);
+  }
+  .swiper-slide-active .cd-btn {
+    /* this is the visible slide */
+    opacity: 1;
+    transform: translateX(0);
   }
 
   @media (max-width: 575px) {
     .swiper-container {
-      width: 100vw;
+      width: 98vw;
     }
   }
 `;
