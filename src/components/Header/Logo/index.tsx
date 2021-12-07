301cda0ce91a3fcc5711d226c9355810729eaf34
Daniel Silva <dani3l.dxp@gmail.com>
2021-12-07 16:30:47 -0300

    feat: criando variáveis de ambiente

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -1,11 +1,12 @@
 import { LogoProps } from "types/api";
+import { getImageUrl } from "utils/getImageUrl";
 
 import * as S from "./styles";
 
 const Logo = ({ alternativeText, url }: LogoProps) => {
   return (
     <S.LogoWrapper
-      src={`http://localhost:1337${url}`}
+      src={getImageUrl(url)}
       alt={alternativeText}
       title={alternativeText}
     />
