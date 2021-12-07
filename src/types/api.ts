88c7809e451f3cd9326f74e8a6dd2d32668e9aaa
Daniel Silva <dani3l.dxp@gmail.com>
2021-12-07 16:56:15 -0300

    feat: criando os typos para banner

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,21 @@
+export type ButtonProps = {
+  label: string;
+  url: string;
+};
+
 export type LogoProps = {
   alternativeText: string;
   url: string;
 };
 
+export type BannerProps = {
+  title: string;
+  description: string;
+  short_description: string;
+  button: ButtonProps;
+};
+
 export type PortfolioProps = {
   logo: LogoProps;
+  banner: BannerProps;
 };
