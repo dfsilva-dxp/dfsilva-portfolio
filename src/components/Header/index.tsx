841f49bfedf0030f49961f91bf0e2b92ae40db59
Daniel Silva <dani3l.dxp@gmail.com>
2021-12-13 10:56:05 -0300

    feat: criando o componene header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,71 +1,17 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-
-import { LogoProps } from "types/api";
-
-import BurgerButton from "components/Header/BurgerButton";
-import FullScreenMenu from "components/Header/FullScreenMenu";
-import NavLinks from "components/Header/NavLinks";
-import Logo from "./Logo";
+import Logo from "../Logo";
+import Navlinks from "../Navlinks";
 
 import * as S from "./styles";
 
-type Props = {
-  logo: LogoProps;
-};
-
-const Header = ({ logo }: Props) => {
-  const [isActive, setIsActive] = useState(false);
-  const [scroll, setScroll] = useState(0);
-
-  const headingRef = useRef<HTMLHeadingElement>(null);
-
-  useEffect(() => {
-    if (headingRef.current) {
-      const offset = 120;
-
-      if (scroll >= offset) {
-        headingRef.current.style.backgroundColor = "#25262A";
-        headingRef.current.style.boxShadow = "0 0 60px rgba(0, 0, 0, 0.05)";
-      } else {
-        headingRef.current.style.backgroundColor = "transparent";
-        headingRef.current.style.boxShadow = "none";
-      }
-    }
-  }, [scroll]);
-
-  useEffect(() => {
-    const updateScroll = () => {
-      setScroll(window.scrollY);
-    };
-    window.addEventListener("scroll", updateScroll);
-
-    return () => window.removeEventListener("scroll", updateScroll);
-  }, []);
-
-  const toggleIsActiveMenu = useCallback(
-    () => setIsActive(!isActive),
-    [isActive]
-  );
-
+function Header() {
   return (
-    <>
-      <S.Header ref={headingRef}>
-        <div className="container">
-          <Logo {...logo} />
-          <NavLinks className="desktop-view" />
-          <BurgerButton
-            className="mobile-view"
-            toggleIsActiveMenu={toggleIsActiveMenu}
-            isActive={isActive}
-          />
-        </div>
-      </S.Header>
-      <FullScreenMenu
-        toggleIsActiveMenu={toggleIsActiveMenu}
-        isActive={isActive}
-      />
-    </>
+    <S.Header>
+      <div className="container">
+        <Logo />
+        <Navlinks />
+      </div>
+    </S.Header>
   );
-};
+}
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,37 +2,13 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   position: fixed;
-  top: 0;
+  background-color: var(--white);
   width: 100%;
   height: var(--px-80);
-  background: transparent;
-  z-index: 10;
+  border-bottom: 1px solid var(--gray-50);
 
-  display: flex;
-  align-items: center;
-  transition: background-color 0.15s ease-in-out;
-
-  .container {
+  & > .container {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-  }
-
-  .mobile-view {
-    display: flex;
-  }
-
-  .desktop-view {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .mobile-view {
-      display: none;
-    }
-
-    .desktop-view {
-      display: block;
-    }
   }
 `;
