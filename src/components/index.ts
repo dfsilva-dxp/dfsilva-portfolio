5988258c6eadfa5263ca95077f76b01f271c36b9
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-26 19:32:57 -0300

    feat: add export default grid

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Base } from "./Base";
 export { default as DottedSquare } from "./DottedSquare";
+export { default as Grid } from "./Grid";
 export { default as Header } from "./Header";
