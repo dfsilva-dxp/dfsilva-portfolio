8e71f00354254d8b78ddef9a500b9ce0713a0985
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-26 21:15:06 -0300

    feat: add about container

---
+++ src/container/about.tsx
@@ -0,0 +1,45 @@
+import { About, Base, Grid, PanelSkill } from "components";
+
+export default function AboutContainer() {
+  return (
+    <Base>
+      <Base.Container>
+        <Base.Grid />
+
+        <About>
+          <Grid>
+            <About.Left>
+              <PanelSkill
+                title="Front-End Developer"
+                imageURL="/img/front-end.svg"
+                brPosition="tr"
+              />
+              <PanelSkill
+                title="Artista de Pixel"
+                imageURL="/img/pixel-perfect.svg"
+              />
+              <PanelSkill
+                title="Prototipagem & Wireframe"
+                imageURL="/img/wireframe.svg"
+                brPosition="bl"
+              />
+            </About.Left>
+            <About.Right>
+              <About.Title>Sobre mim.</About.Title>
+
+              <About.Subtitle>
+                Olá, meu nome é Daniel Francisco da Silva.
+              </About.Subtitle>
+
+              <About.Description>
+                Sou Front-end developer a 4 anos, apaixonado por desenvolvimento
+                com Reactjs, Nextjs e UI Design. Sua próxima ideia já tem um
+                Dev!
+              </About.Description>
+            </About.Right>
+          </Grid>
+        </About>
+      </Base.Container>
+    </Base>
+  );
+}
