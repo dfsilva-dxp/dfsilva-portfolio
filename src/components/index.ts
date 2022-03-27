f0b161795e39bef61f177d7f634d43c59db5661f
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-26 21:14:43 -0300

    feat: export default about and panel skill

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
+export { default as About } from "./About";
 export { default as Base } from "./Base";
 export { default as DottedSquare } from "./DottedSquare";
 export { default as Grid } from "./Grid";
 export { default as Header } from "./Header";
+export { default as PanelSkill } from "./PanelSkill";
