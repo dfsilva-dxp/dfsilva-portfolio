9fe378ebca7756dc258832ca71c8216766e5d5c5
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-27 10:20:49 -0300

    feat: add square svg component

---
+++ src/components/Square/index.tsx
@@ -0,0 +1,53 @@
+import { Container } from "./styles";
+
+export default function Square() {
+  return (
+    <Container>
+      <svg
+        width="63"
+        height="66"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="1.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="19.5" r="1.5" fill="#15131C" />
+        <circle cx="1.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="28.5" r="1.5" fill="#15131C" />
+        <circle cx="1.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="37.5" r="1.5" fill="#15131C" />
+        <circle cx="1.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="46.5" r="1.5" fill="#15131C" />
+        <circle cx="1.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="55.5" r="1.5" fill="#15131C" />
+        <circle cx="1.5" cy="64.5" r="1.5" fill="#15131C" />
+        <circle cx="10.5" cy="64.5" r="1.5" fill="#15131C" />
+        <circle cx="19.5" cy="64.5" r="1.5" fill="#15131C" />
+        <circle cx="28.5" cy="64.5" r="1.5" fill="#15131C" />
+        <circle cx="37.5" cy="64.5" r="1.5" fill="#15131C" />
+        <circle cx="46.5" cy="64.5" r="1.5" fill="#15131C" />
+        <rect x="27" width="36" height="36" rx="5" fill="#0F9" />
+        <rect x="32" y="5" width="26" height="26" rx="5" fill="#08070B" />
+      </svg>
+    </Container>
+  );
+}
---
+++ src/components/Square/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+import * as Dotted from "components/DottedSquare/styles";
+
+export const Container = styled(Dotted.Container)``;
