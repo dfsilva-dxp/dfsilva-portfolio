1c773d03426c8f0c7c4c92d37183cb07f4a58c26
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-29 09:47:18 -0300

    feat: export default experiences and work

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 export { default as About } from "./About";
 export { default as Base } from "./Base";
 export { default as DottedSquare } from "./DottedSquare";
+export { default as Experiences } from "./Experiences";
 export { default as Grid } from "./Grid";
 export { default as Header } from "./Header";
 export { default as PanelSkill } from "./PanelSkill";
 export { default as Square } from "./Square";
+export { default as Work } from "./Work";
