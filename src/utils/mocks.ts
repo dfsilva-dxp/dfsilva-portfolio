779b124cf4891e6ba5a9bd39685c983f2288bb39
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-29 09:51:12 -0300

    feat: add mock with woks data

---
+++ src/utils/mocks.ts
@@ -0,0 +1,26 @@
+export const works = [
+  {
+    title: "Art & Illustratio I",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    repository_url: "https://github.com/daniel-silva-dxp/ignite-ignews"
+  },
+  {
+    title: "Art & Illustratio II",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    repository_url: "https://github.com/daniel-silva-dxp/ignite-ignews"
+  },
+  {
+    title: "Art & Illustratio III",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    repository_url: "https://github.com/daniel-silva-dxp/ignite-ignews"
+  },
+  {
+    title: "Art & Illustratio IV",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    repository_url: "https://github.com/daniel-silva-dxp/ignite-ignews"
+  }
+];
