dfa0e738513d04640004f9637cf8dcc64d8a3f41
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-29 09:48:42 -0300

    feat: import experiences container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { About, Header } from "container";
+import { About, Experiences, Header } from "container";
 
 export default function Home() {
   return (
     <>
       <Header />
       <About />
+      <Experiences />
     </>
   );
 }
