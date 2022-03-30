74672759cf2e794dc8f21a9920ddc98870aa864f
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-30 13:53:02 -0300

    feat: add logo component

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: fit-content;
+  height: fit-content;
+  display: grid;
+  place-content: center;
+
+  svg {
+    width: 12rem;
+  }
+`;
