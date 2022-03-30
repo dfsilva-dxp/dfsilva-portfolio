b939ed4864697d194dfadcb8efbb30e903ccf437
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-30 07:12:15 -0300

    feat: export default fallow me component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as About } from "./About";
 export { default as Base } from "./Base";
 export { default as DottedSquare } from "./DottedSquare";
 export { default as Experiences } from "./Experiences";
+export { default as FallowMe } from "./FallowMe";
 export { default as Grid } from "./Grid";
 export { default as Header } from "./Header";
 export { default as PanelSkill } from "./PanelSkill";
