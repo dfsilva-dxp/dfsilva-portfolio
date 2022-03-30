8b88a24549630198187a57d31581d6f14290e7a8
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-30 07:13:27 -0300

    feat: export default fallow me container

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,3 +1,4 @@
 export { default as About } from "./about";
 export { default as Experiences } from "./experiences";
+export { default as FallowMe } from "./fallowMe";
 export { default as Header } from "./header";
