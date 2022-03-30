d71ca363665aa9c4c674b7514ee172f759a99bbc
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-30 17:55:07 -0300

    feat: export default burguer button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as About } from "./About";
 export { default as Base } from "./Base";
-export { default as BurguerButton } from "./BurguerButton";
+export { default as Burguer } from "./BurguerButton";
 export { default as DottedSquare } from "./DottedSquare";
 export { default as Experiences } from "./Experiences";
 export { default as FallowMe } from "./FallowMe";
