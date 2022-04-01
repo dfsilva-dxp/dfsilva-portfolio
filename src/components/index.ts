2ece7627072c3da898b29518b2e6c323d56deabe
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-01 08:44:39 -0300

    feat: add google analytics

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as About } from "./About";
+export { default as Analytics } from "./Analytics";
 export { default as Base } from "./Base";
 export { default as Burguer } from "./BurguerButton";
 export { default as ContactUs } from "./ContactUs";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
+import * as gtag from "lib/gtag";
+
 import { GlobalStyles, theme } from "styles";
+import { Analytics } from "components";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: unknown) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
