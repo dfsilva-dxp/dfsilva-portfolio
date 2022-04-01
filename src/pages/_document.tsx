7c7037c407a936927086526c55dec6c92fc5a0ee
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-01 15:36:54 -0300

    feat: refatoring

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,7 +49,6 @@ export default class MyDocument extends Document {
             href="/img/icon-512.png"
             type="image/x-icon"
           />
-          <link rel="manifest" href="/manifest.json" />
 
           <title>Daniel Silva &bull; Front-End Developer</title>
           <meta property="og:type" content="website" />
@@ -62,7 +61,6 @@ export default class MyDocument extends Document {
             property="og:description"
             content="Especialista em desenvolvimento Front-End"
           />
-          <meta property="og:image" content="https://dfsilva.dev/" />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://dfsilva.dev/" />
@@ -70,16 +68,12 @@ export default class MyDocument extends Document {
             property="twitter:title"
             content="Daniel Silva - Desenvolvedor Front-End"
           />
-          <meta
-            property="twitter:description"
-            content="Especialista em desenvolvimento Front-End"
-          />
-          <meta property="twitter:image" content="https://dfsilva.dev/" />
 
           <meta
             name="keywords"
             content="front-end, desenvolvedor front-end, front end, daniel silva front end, dfsilva front end, daniel silva, dfsilva, dev, dev front-end, desenvolver sites, programação de sites, portfolio"
           />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
